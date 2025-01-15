b68c2d584149e34a6798166e8c017f4eb17504d4
akm019 <99963365+akm019@users.noreply.github.com>
2025-01-15 18:24:12 +0530

    x

--- Server/index.js
+++ Server/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO with CORS
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173","https://jobseeker-lvk33tn79-aayushs-projects-b2b3ee57.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -33,7 +33,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://jobseeker-lvk33tn79-aayushs-projects-b2b3ee57.vercel.app/"],
   credentials: true
 }));
 app.use(express.json());
